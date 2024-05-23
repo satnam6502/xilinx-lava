3b47b1037e4eeca7f9696a6e3c68674ee505484a
Satnam Singh <satnam6502@gmail.com>
2024-05-23 10:43:16 -0700

    Use CI workflow generated by haskell-ci

--- src/Lava/SimVal.hs
+++ src/Lava/SimVal.hs
@@ -2,6 +2,8 @@
 {-# LANGUAGE GADTs #-}
 {-# LANGUAGE StandaloneDeriving #-}
 {-# LANGUAGE DerivingStrategies #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE InstanceSigs #-}
 module Lava.SimVal where
 
 import qualified Data.BitVector as BV
@@ -16,7 +18,7 @@ data SimVal a where
    L :: SimVal Bool
    H :: SimVal Bool
    BitVec :: BV.BitVector -> SimVal [Bool]
-   Vec :: [SimVal a] -> SimVal ([a])
+   Vec :: [SimVal a] -> SimVal [a]
 
 deriving stock instance Show (SimVal a)
 
@@ -26,3 +28,13 @@ boolVec v = Vec (map boolToSimVal v)
 boolToSimVal :: Bool -> SimVal Bool
 boolToSimVal False = L
 boolToSimVal True = H
+
+class SimValClass a b where
+   toSimVal :: a -> SimVal b
+
+instance SimValClass Int Bool where
+   toSimVal :: Int -> SimVal Bool
+   toSimVal 0 = L
+   toSimVal 1 = H
+   toSimVal other = error ("toSimVal Int Bool is only defined for 0 and 1, not " ++ show other)
+
--- test-verilator/Main.hs
+++ test-verilator/Main.hs
@@ -6,4 +6,4 @@ import Lava.Examples.NandGate
 main :: IO ()
 main
   = do writeSystemVerilog nandGateTop
-       writeSystemVerilogSimulation nandGateTop [[L, L], [H, L], [L, H], [H,H]]
+       writeSystemVerilogSimulation nandGateTop [[L, L], [H, L], [L, H], [H, H]]
