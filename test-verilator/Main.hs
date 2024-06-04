14a3f93377d1089552d2aa0386329f4b2a0936a1
Satnam Singh <satnam6502@gmail.com>
2024-06-03 23:24:55 -0700

    Move verilator calls to inside Haskell test program

--- test-verilator/Main.hs
+++ test-verilator/Main.hs
@@ -1,9 +1,15 @@
 module Main (main) where
 
 import Lava
+import System.Process
+-- import Data.VCD
 import Lava.Examples.NandGate
 
 main :: IO ()
 main
   = do writeSystemVerilog nandGateTop
        writeSystemVerilogSimulation nandGateTop [[L, L], [H, L], [L, H], [H, H]]
+       callProcess "verilator" ["+1800-2017ext+sv", "verilator.vlt", "--binary",  "--trace",  "-Wall", "--top-module",  "nandgate_sim",  "nandgate.sv", "nandgate_sim.sv"]
+       callProcess "obj_dir/Vnandgate_sim" ["+trace"]
+       -- vcd_contents <- readFile "nandgate_sim.vcd"
+       -- putStrLn (show (parseVCD vcd_contents))
