b4c41369f46b0eb3dbab8f2bf687ed07cef2c202
Satnam Singh <satnam6502@gmail.com>
2025-09-08 10:05:23 -0700

    Add adder4.eqy equivalence checking config file

---
+++ src/Lava/Sub4.hs
@@ -0,0 +1,28 @@
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+module Lava.Sub4 where
+
+import Lava
+import Data.Array.Shaped
+
+sub4 :: Hardware m bit => Array '[4] bit -> Array '[4] bit -> m (Array '[4] bit, bit)
+sub4 a b
+  = do ps0 :: bit <- xor2b2 (aL!!0, bL!!0)
+       ps1 :: bit <- xor2b2 (aL!!1, bL!!1)
+       ps2 :: bit <- xor2b2 (aL!!2, bL!!2)
+       ps3 :: bit <- xor2b2 (aL!!3, bL!!3)
+       b0 <- zero
+       b1 <- one
+       carry4 b0 b1 a (fromList [ps0, ps1, ps2, ps3])
+    where
+    aL = toList a
+    bL = toList b
+
+sub4Top :: RTL ()
+sub4Top
+  = do setModuleName "sub4Top"
+       a <- inputVec "a" 4 BitType
+       b <- inputVec "b" 4 BitType
+       (subOut, carryOut) <- sub4 a b
+       outputVec "subOut" subOut BitType
+       output "carryOut" BitType
