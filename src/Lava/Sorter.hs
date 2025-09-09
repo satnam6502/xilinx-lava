3c92a7b5ccda73ad16f169fd0fa0aacd0a668175
Satnam Singh <satnam6502@gmail.com>
2025-09-09 10:05:40 -0700

    Define butterfly

--- src/Lava/Sorter.hs
+++ src/Lava/Sorter.hs
@@ -8,9 +8,13 @@
 {-# LANGUAGE TypeFamilies #-}
 {-# LANGUAGE UndecidableInstances #-}
 {-# LANGUAGE GADTs #-}
+{-# LANGUAGE FlexibleInstances #-}
 
 module Lava.Sorter where
 import GHC.TypeNats
+import Data.Array.Shaped
+
+import Lava.Combinators
 
 -- Define an inductive representation of natural numbers that
 -- can be used to describe the degree of the butterfly.
@@ -33,3 +37,17 @@ mulKNat KNat KNat = KNat
 inatToExpKNat :: INat n -> KNat (ToExpNat n)
 inatToExpKNat Zero = KNat
 inatToExpKNat (Succ n) = mulKNat KNat (inatToExpKNat n)
+
+-- A butterfly of degree n maps 2^n inputs to 2^n outputs.
+class Bfly (n :: N) where
+  bfly :: INat n -> (Array '[2] a -> Array '[2] a) -> Array '[ToExpNat n] a -> Array '[ToExpNat n] a
+
+-- A butterfly of degree 1 is just the r component i.e. it maps 2 inputs to 2 outputs.
+instance Bfly (S Z) where
+  bfly (Succ Zero) r a = r a
+
+-- A butterfly of degree n is recursively defined in terms of two smaller butterflies of degree (n-1).
+instance {-# OVERLAPS #-} (Bfly n) => Bfly (S n) where
+  bfly (Succ n) r = case inatToExpKNat n of
+    KNat -> evens r . ilv (bfly n r)
+
