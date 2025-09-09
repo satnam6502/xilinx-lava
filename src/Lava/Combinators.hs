b60d7bc5da68005122a38c4657d5745b36c58821
Satnam Singh <satnam6502@gmail.com>
2025-09-09 09:42:57 -0700

    Add more combinators: ar, halve, unhalve, pair, unpair, zipA

--- src/Lava/Combinators.hs
+++ src/Lava/Combinators.hs
@@ -1,3 +1,5 @@
+{-# OPTIONS_GHC -fplugin GHC.TypeLits.KnownNat.Solver #-}
+{-# OPTIONS_GHC -fplugin GHC.TypeLits.Normalise #-}
 {-# LANGUAGE RecursiveDo #-}
 {-# LANGUAGE KindSignatures #-}
 {-# LANGUAGE DataKinds #-}
@@ -5,10 +7,13 @@
 {-# LANGUAGE RankNTypes #-}
 {-# LANGUAGE NoStarIsType #-}
 {-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE TypeApplications #-}
+{-# LANGUAGE TypeOperators #-}
+{-# LANGUAGE FlexibleContexts #-}
 
 -- | Generic combinators for system construction.
 
-module Lava.Combinators (module Control.Monad, fork, loop, par)
+module Lava.Combinators (module Control.Monad, fork, loop, par, halve, unhalve, pair, unpair, zipA)
 where
 import Control.Monad.Fix (MonadFix)
 import Control.Monad ((>=>)) 
@@ -34,3 +39,18 @@ par :: (KnownNat n, Monad m) => (a -> m b) -> Array '[n] a -> m (Array '[n] b)
 par f a
   = traverseA id (mapA f a)
 
+
+halve :: forall a n. (KnownNat n, KnownNat (2*n)) => Array '[2 * n] a -> Array '[2] (Array '[n] a)
+halve = unravel . reshape @'[2, n]
+
+unhalve :: forall a n. KnownNat n => Array '[2] (Array '[n] a) -> Array '[2 * n] a
+unhalve = reshape @'[2 * n] . ravel
+
+pair :: forall a n. KnownNat n => Array '[2 * n] a -> Array '[n] (Array '[2] a)
+pair = unravel . reshape @'[n, 2]
+
+unpair :: forall a n. KnownNat n => Array '[n] (Array '[2] a) -> Array '[2 * n] a
+unpair = reshape @'[2 * n] . ravel
+
+zipA :: forall a n. KnownNat n => Array '[2] (Array '[n] a) -> Array '[n] (Array '[2] a)
+zipA =  unravel . transpose @[1, 0] . ravel
